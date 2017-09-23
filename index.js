7c3257a53ff0cc77a4345a4d534c177df01ae603
arctair <jtyler_@outlook.com>
2017-09-23 02:59:48 -0500

    route/pathing

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const router = express.Router();
 const path = require('path');
 const request = require('request');
 const app = express();
 
-app.use('/leaflet', express.static(path.join(__dirname, '/node_modules/leaflet/dist')));
-app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.use('/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
-app.use('/turf', express.static(path.join(__dirname, '/node_modules/@turf/turf')));
-app.use('/public', express.static(path.join(__dirname, '/public')));
+router.use('/leaflet', express.static(path.join(__dirname, '/node_modules/leaflet/dist')));
+router.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+router.use('/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
+router.use('/turf', express.static(path.join(__dirname, '/node_modules/@turf/turf')));
+router.use('/public', express.static(path.join(__dirname, '/public')));
 
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/colormap.html')));
-app.get('/json', (req, res) => {
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '/colormap.html')));
+router.get('/json', (req, res) => {
   request.get('http://maps2.dnr.state.mn.us/cgi-bin/fall_colors_json.cgi', (err, response, body) => {
     if (err || response.statusCode != 200) {
       res.status(response.statusCode).send(err);
@@ -22,5 +23,7 @@ app.get('/json', (req, res) => {
     }
   });
 });
+
+app.use('/', router);
 
 app.listen(3000, () => console.log("*:3000"));
