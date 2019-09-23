de67489b87958f4a76bcb31ac3c7cd9013da1181
arctair <jtyler_@outlook.com>
2019-09-23 18:43:33 -0500

    generate tif on the fly

--- createTif.sh
+++ createTif.sh
@@ -1,12 +1,14 @@
-for zfield in leaves flowers grasses
-do
-gdal_grid -zfield $zfield \
+# $1 geojson in
+# $2 cutline in
+# $3 zfield
+# $4 tif out
+tmp="$(mktemp /tmp/XXXXXX.tif)"
+gdal_grid -zfield $3 \
   -a invdist:power=3:smoothing=0.25:radius1=0.0:radius2=0.0:angle=0.0:max_points=6:min_points=0:nodata=0.0 \
-  fall.geojson $zfield-idw.tif
+  $1 $tmp
 gdalwarp -dstalpha \
   -srcnodata 0 -dstnodata 0 \
-  -cutline mn/mn.shp \
+  -cutline $2 \
   -crop_to_cutline \
-  $zfield-idw.tif $zfield.tif
-rm $zfield-idw.tif
-done
+  $tmp $4
+rm $tmp
--- server.js
+++ server.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const rq = require('request-promise');
+const mktemp = require('mktemp');
+const fs = require('fs').promises;
+const exec = require('child_process').exec;
 
 const app = express();
 
-app.post('/methods/update', (request, response) => {
-  rq(process.env.DNR_CGI_URL)
-  .then(deduplicateCommas)
-  .then(extractJsonList)
-  .then(JSON.parse)
-  .then(createDnrFallGeoJson)
-  .then(featureCollection => ({
-    ...featureCollection,
-    features: featureCollection.features.sort((
-      { properties: { id: idFirst } },
-      { properties: { id: idLast } },
-    ) => idFirst.localeCompare(idLast)),
-  }))
-  .then(JSON.stringify)
-  .then(s => response.send(s));
+app.get('/', async (_, response) => {
+  try {
+    const body = JSON.stringify(await getGeoJson());
+    const geojsonPath = await mktemp.createFile('/tmp/XXXXXX.geojson');
+    console.log('geojsonPath', geojsonPath);
+    const tifPath = await mktemp.createFile('/tmp/XXXXXX.tif');
+    console.log('tifPath', tifPath);
+    await fs.writeFile(geojsonPath, body);
+    await new Promise((resolve, reject) => {
+      exec(`sh createTif.sh ${geojsonPath} mn/mn.shp leaves ${tifPath}`, (error, stdout, stderr) => {
+        if (error) reject(error);
+        resolve(stdout);
+      });
+    });
+    await new Promise(resolve => response.sendFile(tifPath, resolve));
+    await Promise.all([fs.unlink(geojsonPath), fs.unlink(tifPath)]);
+    console.log('unlink complete')
+  }
+  catch (error) {
+    response.status(500);
+    response.send(error);
+  }
 });
+
+app.get('/geojson', async (_, response) => response.json(await getGeoJson()));
+
+const getGeoJson = () => rq(process.env.DNR_CGI_URL)
+.then(deduplicateCommas)
+.then(extractJsonList)
+.then(JSON.parse)
+.then(createDnrFallGeoJson)
+.then(featureCollection => ({
+  ...featureCollection,
+  features: featureCollection.features.sort((
+    { properties: { id: idFirst } },
+    { properties: { id: idLast } },
+  ) => idFirst.localeCompare(idLast)),
+}))
 
 const deduplicateCommas = s => s.replace(/,+/g, ',');
 
